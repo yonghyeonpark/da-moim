af4a83ca52d55cfb76509034768d1c44d462c166
yonghyeonpark <pyh4616@naver.com>
2024-07-30 01:34:12 +0900

    feat: UserDetails의 구현체 생성

---
+++ src/main/java/community/da_moim/service/auth/dto/UserDetailsImpl.java
@@ -0,0 +1,32 @@
+package community.da_moim.service.auth.dto;
+
+import community.da_moim.domain.user.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private User user;
+
+    public UserDetailsImpl(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getLoginId();
+    }
+}
