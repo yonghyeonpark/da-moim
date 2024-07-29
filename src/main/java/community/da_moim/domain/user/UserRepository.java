03da1355225e855db31efc4ee6fb13df4d74717f
yonghyeonpark <pyh4616@naver.com>
2024-07-30 01:36:22 +0900

    feat: loadUserByUsername에 사용하기 위한 메서드 구현

--- src/main/java/community/da_moim/domain/user/UserRepository.java
+++ src/main/java/community/da_moim/domain/user/UserRepository.java
@@ -3,4 +3,6 @@ package community.da_moim.domain.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByLoginId(String loginId);
 }
