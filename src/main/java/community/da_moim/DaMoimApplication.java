868d576a327302662367ac9f07f2eeda140b1dac
yonghyeonpark <pyh4616@naver.com>
2024-07-29 20:43:27 +0900

    feat: 엔터티 생성 및 변경 시간의 추적을 위한 설정
    - BaseTimeEntity 구현
    - Application 파일에 어노테이션 추가

--- src/main/java/community/da_moim/DaMoimApplication.java
+++ src/main/java/community/da_moim/DaMoimApplication.java
@@ -2,12 +2,13 @@ package community.da_moim;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class DaMoimApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DaMoimApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DaMoimApplication.class, args);
+    }
 }
---
+++ src/main/java/community/da_moim/domain/BaseTimeEntity.java
@@ -0,0 +1,27 @@
+package community.da_moim.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedAt;
+
+    @Column
+    private LocalDateTime deletedAt;
+}
