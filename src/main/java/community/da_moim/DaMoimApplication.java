d116be65416d8931c2feec8e14373486944b21bb
yonghyeonpark <pyh4616@naver.com>
2024-07-29 16:12:13 +0900

    chore: initial settings

---
+++ src/main/java/community/da_moim/DaMoimApplication.java
@@ -0,0 +1,13 @@
+package community.da_moim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaMoimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaMoimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/community/da_moim/DaMoimApplicationTests.java
@@ -0,0 +1,13 @@
+package community.da_moim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaMoimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
