df81d8e08dce356016ba6c3a7fcbcd7251492f8d
yonghyeonpark <pyh4616@naver.com>
2024-07-31 13:04:00 +0900

    feat: User 엔터티와 Dto 변환을 위한 Mapper 구현

---
+++ src/main/java/community/da_moim/util/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package community.da_moim.util.mapper;
+
+import community.da_moim.domain.user.User;
+import community.da_moim.web.user.dto.request.UserSaveDto;
+
+public class UserMapper {
+
+    public static User toEntity(UserSaveDto userSaveDto) {
+        return new User(
+                userSaveDto.getLoginId(),
+                userSaveDto.getPassword(),
+                userSaveDto.getNickname(),
+                userSaveDto.getEmail(),
+                userSaveDto.getPhoneNumber()
+        );
+    }
+}
