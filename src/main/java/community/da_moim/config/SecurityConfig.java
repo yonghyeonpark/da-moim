99fcb7fd79de449e4acf0de5e5e894078489d61c
yonghyeonpark <pyh4616@naver.com>
2024-08-01 19:02:57 +0900

    fix: 스프링 시큐리티의 기본 로그인 기능 사용을 위한 설정 추가

--- src/main/java/community/da_moim/config/SecurityConfig.java
+++ src/main/java/community/da_moim/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -25,6 +26,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/posts").authenticated()
                         .anyRequest().permitAll()
                 );
+        http.formLogin(AbstractAuthenticationFilterConfigurer::permitAll);
         return http.build();
     }
 }
