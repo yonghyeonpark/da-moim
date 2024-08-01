1c550f12f0132f8d0fa8c94446487395b5ec8019
yonghyeonpark <pyh4616@naver.com>
2024-08-01 17:15:34 +0900

    feat: 게시글 수정 요청 매핑을 위한 Dto 구현

---
+++ src/main/java/community/da_moim/web/post/dto/request/PostUpdateDto.java
@@ -0,0 +1,19 @@
+package community.da_moim.web.post.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class PostUpdateDto {
+
+    @NotBlank
+    @Size(max = 200)
+    private final String title;
+
+    @NotBlank
+    @Size(max = 1000)
+    private final String content;
+}
