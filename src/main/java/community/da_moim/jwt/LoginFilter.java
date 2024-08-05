b4207b5e482c48497a6055a0f2388c306cd17a6a
yonghyeonpark <pyh4616@naver.com>
2024-08-05 16:31:58 +0900

    feat: 토큰 인증 성공 및 실패 로직 구현

---
+++ src/main/java/community/da_moim/jwt/LoginFilter.java
@@ -0,0 +1,40 @@
+package community.da_moim.jwt;
+
+import community.da_moim.service.auth.dto.UserDetailsImpl;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        String loginId = obtainUsername(request);
+        String password = obtainPassword(request);
+        return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginId, password)); // 인자로 Role도 추가 가능
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        UserDetailsImpl userDetails = (UserDetailsImpl) authResult.getPrincipal();
+        response.addHeader("Authorization", "Bearer " + jwtUtil.createJwt(userDetails.getUsername(), 60 * 60 * 10L));
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        response.setStatus(401);
+    }
+}
